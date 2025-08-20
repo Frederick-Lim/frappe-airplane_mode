0177fb6f2090d5f4c73e1d8d5cd23df0843947d7
Frederick Lim <hungwai.lim@gmail.com>
2025-08-20 13:26:44 +0000

    feat: Set seat in Airplane Ticket

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -1,7 +1,8 @@
 # Copyright (c) 2025, Frederick Lim and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+import random
 from frappe.model.document import Document
 
 
@@ -12,18 +13,21 @@ class AirplaneTicket(Document):
 
 	def calculate_total_amount(self):
 		# Calculate total amount based on flight_price and the sum of add_ons items
-		self.total_amount = self.flight_price + sum(item.amount for item in self.add_ons) or 0
+		# self.total_amount = self.flight_price + (sum(item.amount for item in self.add_ons) or 0)
+		total_add_ons = sum(item.amount for item in self.add_ons) or 0
+		self.total_amount = self.flight_price + total_add_ons
+
 
 	def remove_duplicate_item(self):
-		# Set to track seen items and rows for removal
+		# Track seen items and rows for removal
 		seen_items = set()
-		rows_to_remove = set()
+		rows_to_remove = []
 
 		# Iterate through child table rows
 		for row in self.add_ons:
 			if row.item in seen_items:
 				# Mark this row for removal (duplicate)
-				rows_to_remove.add(row)
+				rows_to_remove.append(row)
 			else:
 				# Mark this item as seen
 				seen_items.add(row.item)
@@ -31,3 +35,16 @@ class AirplaneTicket(Document):
 		# Remove duplicates from the child table
 		for row in rows_to_remove:
 			self.remove(row)
+
+	def before_submit(self):
+		self.check_status()
+
+	def check_status(self):
+		if self.status != "Boarded":
+			frappe.throw('Ticket Status must be "Boarded"')
+
+	def before_insert(self):
+		self.set_seat()
+		
+	def set_seat(self):
+		self.seat = f"{random.randint(1, 60)}{random.choice('ABCDE')}"
